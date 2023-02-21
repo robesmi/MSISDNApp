a8295e2f8e28c868e8c87909702c3c4dc5a9e43d
Robert-Smiljkovski <smiljkovski.robert@gmail.com>
2023-02-21 15:19:36 +0100

    Data model redefining for future implementation

--- model/Country.go
+++ model/Country.go
@@ -1,6 +1,13 @@
 package model
 
 type Country struct {
+	// CountryCode is a regex representing the country code and format
+	// of each country, identified by the first 1-4 digits
 	CountryCode string 
+	// CountryIdentifier is a ISO 3166-1-alpha-2 format of the country
+	// the MSISDN belongs to
 	CountryIdentifier string
+	// CountryCodeLength is the length of the CountryCode, used to 
+	// trim away the unneeded country code in following queries
+	CountryCodeLength int
 }
--- model/MobileOperator.go
+++ model/MobileOperator.go
@@ -1,7 +1,16 @@
 package model
 
 type MobileOperator struct {
+	// CountryIdentifier is a ISO 3166-1-alpha-2 format of the country
+	// the MSISDN belongs to 
 	CountryIdentifier string
-	Prefix int
+	// Prefix is a regex to distinguish the NCD and format of numbers
+	// for each MNO
+	Prefix string
+	// MNO is the name of the MNO the regex applies to
 	MNO string
+	// PrefixLength is the length of the MNO's carrier code, used to
+	// trim away the unneeded carrier code to isolate the subscriber number
+	PrefixLength int
+
 }
