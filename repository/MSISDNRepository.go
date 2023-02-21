92cd1f2d9ee55124a029ede4778892b336a7c733
Robert-Smiljkovski <smiljkovski.robert@gmail.com>
2023-02-21 11:53:31 +0100

    Change return type

--- repository/MSISDNRepository.go
+++ repository/MSISDNRepository.go
@@ -14,16 +14,16 @@ func NewMSISDNRepository(dbClient *sqlx.DB) MSISDNRepositoryDb{
 }
 
 type MSISDNRepository interface{
-	LookupCountryCode(int) (string, *errs.AppError)
-	LookupMobileOperator(string, int) (string, *errs.AppError)
+	LookupCountryCode(int) (*string, *errs.AppError)
+	LookupMobileOperator(string, int) (*string, *errs.AppError)
 }
 
 // LookupCountryCode takes a int country code and returns the respective country identifier it belongs to
-func (repo MSISDNRepositoryDb) LookupCountryCode(cc int) (string, *errs.AppError){	
+func (repo MSISDNRepositoryDb) LookupCountryCode(cc int) (*string, *errs.AppError){	
 	panic("panic!!")
 }
 
 // LookupMobileOperator takes a country identifier and a NCD/NPA code and returns an MNO
-func (repo MSISDNRepositoryDb) LookupMobileOperator(cc string, prefix int) (string, *errs.AppError){
+func (repo MSISDNRepositoryDb) LookupMobileOperator(cc string, prefix int) (*string, *errs.AppError){
 	panic("panic!!")
 }
